36607f3c7956f92b7686ac40885a3c9337de9ec4
cawilson1 <cawilson1@g.cofc.edu>
2020-02-02 14:19:41 -0500

    Updates

---
+++ precache-manifest.d331f6a1fd2d81ff417513cb93c7e786.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e8ce8a0fd537f7f671a7dc59778e266",
+    "url": "/temp2/index.html"
+  },
+  {
+    "revision": "d17a38e7c52cce211f53",
+    "url": "/temp2/static/css/main.40222aeb.chunk.css"
+  },
+  {
+    "revision": "9e2bce004ec7ab3deb63",
+    "url": "/temp2/static/js/2.f8a3866d.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/temp2/static/js/2.f8a3866d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d17a38e7c52cce211f53",
+    "url": "/temp2/static/js/main.a038db9d.chunk.js"
+  },
+  {
+    "revision": "04170661b133b58a9a45",
+    "url": "/temp2/static/js/runtime-main.ba6f65bf.js"
+  }
+]);
